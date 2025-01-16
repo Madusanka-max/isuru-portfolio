2d58c4108f036bdc3a31f302e9664392fc4de28a
Isuru Madusanka Rodrigo <66762109+Madusanka-max@users.noreply.github.com>
2025-01-16 22:35:30 +0530

    0.3.1

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,15 @@
+"use client";
+
+import react, {useState} from "react";
+import {motion} from "framer-motion";
+
+import {Swiper,SwiperSlide} from "swiper/react";
+import"swiper/css";
+
+import { BsArrowUpRight,BsGithub } from "react-icons/bs";
+
+import { Tooltip,TooltipContent,TooltipProvider,TooltipTrigger, } from "@radix-ui/react-tooltip";
+import Link from "next/link";
 const work = () => {
   return (
     <div>work page</div>
