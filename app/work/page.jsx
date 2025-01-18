c2a9925cb0685a3126be44d41e3198b43cc68868
Isuru Madusanka Rodrigo <66762109+Madusanka-max@users.noreply.github.com>
2025-01-18 17:44:05 +0530

    0.3.4

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -37,16 +37,24 @@ const projects=[
 ];
 const work = () => {
   const[project,setProject]=useState(projects[0]);
+
+  const handleSlideChange=(swiper)=>{
+    // get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setProject(projects[currentIndex]);
+  }
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }}}
       className="mini-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div>
+            <div className="flex flex-col gap-[30px] h-[50%]">
               {/* outline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
               {/* project category */}
@@ -66,16 +74,29 @@ const work = () => {
               {/* border */}
               <div className="border border-white/20"></div>
               {/* buttons */}
-              <div>
-                {/*  */}
+              <div className="flex items-center gap-4">
+                {/* live project button */}
                 <Link href={project.live}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 justify-center items-center group">
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
                         <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
                       </TooltipTrigger>
                       <TooltipContent>
-                        <p>Live Project</p>
+                        <p className="p-2 rounded bg-white text-black">Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* github project button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className="p-2 rounded bg-white text-black">GitHub repository</p>
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
@@ -83,7 +104,23 @@ const work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+              {projects.map((project, index) => {
+                return <SwiperSlide key={index} className="w-full">
+                  <div className="h-[400px] relative group flex justify-center items-center bg-pink-50/20">
+                    {/* overlay */}
+                    <div></div>
+                    {/* image */}
+                    <div className="relative w-full h-full">
+                      <Image src={project.image} fill className="object-cover" alt="" />
+                    </div>
+                    
+                  </div>
+                </SwiperSlide>;
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
